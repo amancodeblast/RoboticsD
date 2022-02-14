a01ad35586ef545298e0e11382c6ac1e7eb37f9c
amancodeblast <sharma.aman7698@gmail.com>
2022-02-14 09:39:42 -0800

    Particle filter - assigning importance weights

--- localisation/exercises/monte_carlo/motion_and_sensing.cpp
+++ localisation/exercises/monte_carlo/motion_and_sensing.cpp
@@ -235,22 +235,39 @@ int main()
     myrobot.move(-M_PI / 2.0, 10.0);
     //cout << myrobot.read_sensors() << endl;
 
-    //####   DON'T MODIFY ANYTHING ABOVE HERE! ENTER CODE BELOW ####
-
-    // Instantiating 1000 Particles each with a random position and orientation
+    // Create a set of particles
     int n = 1000;
     Robot p[n];
-    //TODO: Your job is to loop over the set of particles
-    //TODO: For each particle add noise: Forward_Noise=0.05, Turn_Noise=0.05, and Sense_Noise=5.0
-    //TODO: And print its pose on a single line
-    for(int i=0;i<n;i++){
-        p[i].set_noise(0.05,0.05,5.0);
-        
-        cout<<p[i].show_pose() <<endl;
+
+    for (int i = 0; i < n; i++) {
+        p[i].set_noise(0.05, 0.05, 5.0);
+        //cout << p[i].show_pose() << endl;
     }
-    
-    
-    
-    
+
+    //Re-initialize myrobot object and Initialize a measurment vector
+    myrobot = Robot();
+    vector<double> z;
+
+    //Move the robot and sense the environment afterwards
+    myrobot = myrobot.move(0.1, 5.0);
+    z = myrobot.sense();
+
+    // Simulate a robot motion for each of these particles
+    Robot p2[n];
+    for (int i = 0; i < n; i++) {
+        p2[i] = p[i].move(0.1, 5.0);
+        p[i] = p2[i];
+    }
+
+    //####   DON'T MODIFY ANYTHING ABOVE HERE! ENTER CODE BELOW ####
+
+    //TODO: Generate particle weights depending on robot's measurement
+    //TODO: Print particle weights, each on a single line
+    double weights[n];
+    for(int i=0; i<n;i++){
+        weights[i]=p[i].measurement_prob(z);
+        cout<<weights[i]<<endl;
+    }
+
     return 0;
 }
